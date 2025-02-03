250655d7b6904136267d0b15644a968c8149add5
RiyadhBenhallou <riadhriadh2004@gmail.com>
2025-02-03 08:52:26 -0800

    Refactor Features and GetApp components; adjust image dimensions, remove unnecessary borders, and enhance layout for improved responsiveness

--- components/features.tsx
+++ components/features.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 
 export default function Features() {
   return (
-    <section className="border-2 border-red-500 py-24 bg-feature-bg bg-center gb-cover bg-no-repeat justify-end">
+    <section className="border-2 py-24 bg-feature-bg bg-center gb-cover bg-no-repeat justify-end overflow-hidden">
       <div className="container mx-auto relative w-full flex">
-        <div className="md:flex flex-1 min-h-[900px] hidden">
+        <div className="md:flex flex-1 min-h-[700px] hidden">
           <Image
             src="/phone.png"
             alt="phone"
-            width={440}
-            height={1000}
+            width={390}
+            height={800}
             className="feature-phone"
           />
         </div>
-        <div className="flex flex-col gap-1 lg:w-[60%] relative border">
+        <div className="flex flex-col gap-1 lg:w-[60%] relative">
           <div>
             <Image
               src="/camp.svg"
@@ -25,7 +25,7 @@ export default function Features() {
             />
             <h2 className="text-4xl font-bold">Our Features</h2>
           </div>
-          <ul className="space-y-2 mt-12">
+          <ul className="mt-16 grid grid-cols-1 md:grid-cols-2 gap-8">
             {FEATURES.map((f, i) => (
               <FeatureItem
                 key={i}
@@ -50,11 +50,13 @@ const FeatureItem = ({
   description: string;
   icon: string;
 }) => {
-  return <li className="flex flex-col gap-4 felx-1 w-full items-start">
-    <div className="rounded-full p-3 bg-green-500 flex justify-center items-center">
-      <Image src={icon} alt={title} width={34} height={34} />
-    </div>
-    <h2 className="text-3xl font-bold">{title}</h2>
-    <p className="font-light text-gray-600">{description}</p>
-  </li>;
+  return (
+    <li className="flex flex-col gap-4 felx-1 w-full items-start">
+      <div className="rounded-full p-3 bg-green-500 flex justify-center items-center">
+        <Image src={icon} alt={title} width={34} height={34} />
+      </div>
+      <h2 className="text-3xl font-bold">{title}</h2>
+      <p className="font-light text-gray-600">{description}</p>
+    </li>
+  );
 };
